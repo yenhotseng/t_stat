270e1b09bc577e5b1e8f2389ed5978c9c2fbc90f
yenhotseng <yenho@verdigris.co>
2018-10-18 13:34:24 +0800

    Add gdaq test shell script

---
+++ run_gdaq.sh
@@ -0,0 +1,11 @@
+
+
+systemctl stop gdaq
+sleep 1
+systemctl start gdaq
+dmesg | tail
+cat /sys/bus/iio/devices/iio\:device0/sampling_frequency
+cat /sys/bus/iio/devices/iio\:device0/sampling_frequency
+cat /sys/bus/iio/devices/iio\:device0/sampling_frequency
+cat /sys/bus/iio/devices/iio\:device0/sampling_frequency
+cat /sys/bus/iio/devices/iio\:device0/sampling_frequency
