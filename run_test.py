42ced2a9ca182f557883b4041f0f08e3fdb28a70
yenhotseng <yenho@verdigris.co>
2018-10-18 10:43:02 +0800

    Init t-test original python code

---
+++ run_test.py
@@ -0,0 +1,2 @@
+import os
+os.system('/root/run_dump.sh')
---
+++ stat_t.py
@@ -0,0 +1,46 @@
+# t-test for independent samples
+from math import sqrt
+from numpy.random import seed
+from numpy.random import randn
+from numpy import mean
+from scipy.stats import sem
+from scipy.stats import t
+
+# function for calculating the t-test for two independent samples
+def independent_ttest(data1, data2, alpha):
+	# calculate means
+	mean1, mean2 = mean(data1), mean(data2)
+	# calculate standard errors
+	se1, se2 = sem(data1), sem(data2)
+	# standard error on the difference between the samples
+	sed = sqrt(se1**2.0 + se2**2.0)
+	# calculate the t statistic
+	t_stat = (mean1 - mean2) / sed
+	# degrees of freedom
+	df = len(data1) + len(data2) - 2
+	# calculate the critical value
+	cv = t.ppf(1.0 - alpha, df)
+	# calculate the p-value
+	p = (1.0 - t.cdf(abs(t_stat), df)) * 2.0
+	# return everything
+	return t_stat, df, cv, p
+
+# seed the random number generator
+seed(1)
+# generate two independent samples
+data1 = 5 * randn(100) + 50
+data2 = 5 * randn(100) + 51
+# calculate the t test
+alpha = 0.05
+t_stat, df, cv, p = independent_ttest(data1, data2, alpha)
+print('t=%.3f, df=%d, cv=%.3f, p=%.3f' % (t_stat, df, cv, p))
+# interpret via critical value
+if abs(t_stat) <= cv:
+	print('Accept null hypothesis that the means are equal.')
+else:
+	print('Reject the null hypothesis that the means are equal.')
+# interpret via p-value
+if p > alpha:
+	print('Accept null hypothesis that the means are equal.')
+else:
+	print('Reject the null hypothesis that the means are equal.')
